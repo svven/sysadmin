31d2c1121c4fc811aaaa855e87918ecf7aceeb1d
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-19 18:44:24 +0000

    Fix locale within update

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ System administration scripts working on Ubuntu.
 Run a script to view its required and optional arguments.
 
 * `adduser.sh` - add user to group
-* `fixlocale.sh` - fix locale before updates
 * `setssh.sh` - set SSH keys for running user
+* `update.sh` - fix locale and update
--- update.sh
+++ update.sh
@@ -5,3 +5,10 @@ export LC_ALL=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 echo "LC_ALL=\"en_US.UTF-8\"" | sudo tee -a /etc/environment /etc/default/locale
 echo "LANGUAGE=\"en_US.UTF-8\"" | sudo tee -a /etc/environment /etc/default/locale
+
+## Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+## Major requirements
+sudo apt-get install -y build-essential git
